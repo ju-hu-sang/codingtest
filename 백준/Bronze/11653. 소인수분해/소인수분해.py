36b3c78489bf53095ba78e54a86d15e8ab2dcf43
uncle chu <imhusang@gmail.com>
2025-01-23 16:43:01 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+N = int(input())
+if N > 1:  
+    factor = 2
+    while factor * factor <= N:  
+        while N % factor == 0:  
+            print(factor)  
+            N //= factor  
+        factor += 1  
+
+    if N > 1:  
+        print(N)
