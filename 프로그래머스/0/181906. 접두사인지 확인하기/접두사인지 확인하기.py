6491f321b09faf2d39f0b43acb7e943115f44671
후추성님 <imhusang@gmail.com>
2025-07-09 13:10:49 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,8 +1,2 @@
 def solution(my_string, is_prefix):
-    answer = []
-    for i in range(len(my_string)):
-        answer.append(my_string[:i])
-    if is_prefix in sorted(answer):
-        return 1
-    else:
-        return 0
+    return int(my_string.startswith(is_prefix))
